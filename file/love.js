88d7bca1a985d848c067d231409c6698a84b95fc
Dhairya <dhairyabhanushali3535@gmail.com>
2026-07-08 20:08:41 +0530

    Update love.js

--- file/love.js
+++ file/love.js
@@ -176,7 +176,7 @@
             var ctx = this.tree.ctx, cirle = this.cirle;
             var point = cirle.point, scale = cirle.scale, radius = 26;
             var w = h = (radius * scale);
-            ctx.clearRect(point.x - w, point.y - h, 4 * w, 4 * h)
+            ctx.clearRect(point.x - w, point.y - h, 5 * w, 5 * h)
         },
         hover: function(x, y) {
             var ctx = this.tree.ctx;
